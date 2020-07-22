b263e6b9241a0c412d6918f927a9296fc1aa6926
macT <simmonst208@gmail.com>
2020-07-21 22:14:19 -0600

    functions for specified roles is completed

--- Develop/app.js
+++ Develop/app.js
@@ -5,18 +5,42 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+//need to do, use filesystem to create Path.. output is a directory then create file that is team.html fileSync
+const OUTPUT_DIR = path.resolve(__dirname, "output"); //fs.mkdirSync
+const outputPath = path.join(OUTPUT_DIR, "team.html"); //fs.filesync with path and content is return of render function store render function into a variable and pass it through write file sync call as last thing
 
 const render = require("./lib/htmlRenderer");
+const teamManager;
+const team = [];
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+const questions = [{
+    type: "input",
+    message: "what is your name?",
+    name: "name"
+}];
+inquirer.prompt(questions).then(answers => {
+
+        // function to create manager, always a manager 
+        
+
+        //create engineer
+
+        //create intern
+
+        //function to create whole team
+            //switch role use role to execute creation of team member
+              
+})
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
+
+render();
 
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,20 @@
+const Employee = require("./Employee");
+
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee2 = require("./Employee.js");
+
+class Engineer extends Employee2 {
+    constructor(name, id, email, github) {
+        super(name, id, email);
+        this.github = github;
+    }
+
+    getGithub(){
+        return this.github;
+    }
+    getRole() {
+        return "Engineer";
+    }
+}
+
+module.exports = Engineer;
